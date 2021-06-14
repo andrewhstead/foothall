11254189b1a4005d350418be80fcd8188597eef8
Andrew Stead <andrew@andrewstead.co.uk>
2021-06-14 20:00:43 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="en-GB">
+
+	<!-- Header information for the page. -->
+	<head>
+		<title>The FootHall - an International Football Hall of Fame Voted by the Public</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="/favicon.ico">
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	
+	<!-- The visible content of the page. -->
+	<body>
+		
+		<header>
+			The FootHall
+		</header>
+	
+	</body>
+
+</html>
