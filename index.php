fa1834e58a851816a8b53993257cf14af8ca5c4b
Andrew Stead <andrew@andrewstead.co.uk>
2021-06-14 23:41:23 +0100

    Created static navigation menu.

--- index.php
+++ index.php
@@ -30,7 +30,28 @@
 		<nav class="main-menu">
 			
 			<!-- Menu icon to toggle menu using JavaScript. -->
-			<div class="menu-toggle" onclick="menuToggle('menu')">MENU &#9776;</div>	
+			<div class="menu-toggle" onclick="menuToggle('menu')">MENU &#9776;</div>
+			
+			<ul class="navigation">
+				<li class="menu-link">Home</li>
+				<li>+ The Hall of Fame
+					<ul class="navigation">
+						<li class="menu-link">Voting</li>
+						<li class="menu-link">Players</li>
+						<li class="menu-link">Coaches</li>
+						<li class="menu-link">Matches</li>
+						<li class="menu-link">Teams</li>
+					</ul>
+				</li>
+				<li>+ The History of Football
+					<ul class="navigation">
+						<li class="menu-link">Tournaments</li>
+						<li class="menu-link">Countries</li>
+						<li class="menu-link">Clubs</li>
+						<li class="menu-link">Stories</li>
+					</ul>
+				</li>
+			</ul>
 		
 		</nav>
 		
