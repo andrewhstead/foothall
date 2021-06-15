9b89aa0b32a12f9ad249a6aee405dba4462cd26c
Andrew Stead <andrew@andrewstead.co.uk>
2021-06-15 23:55:04 +0100

    Extended Home Page feed to cover multiple categories of article.

--- index.php
+++ index.php
@@ -9,28 +9,70 @@
 	
 	$polls = "SELECT * FROM polls";
 	$poll_content = $connectDB->query($polls);
+	$people = "SELECT * FROM people";
+	$people_content = $connectDB->query($people);
 
-	while ($dataRows = $content->fetch()) {
+	while ($dataRows = $poll_content->fetch()) {
 
-		$poll_id = $dataRows["id"];
-		$poll_title = $dataRows["title"];
-		$poll_intro = $dataRows["intro_text"];
-		$poll_posted = $dataRows["date_time"];
-		$poll_expires = $dataRows["expiry"];
-?>			
+		$content[] = $dataRows;
 		
-	<div class="feed-post">
-		
-		<a href="polls.php?id=<?php echo htmlentities($poll_id); ?>">
-			<?php echo htmlentities($poll_title); ?>
-		</a><br>
-		<?php echo htmlentities($poll_intro); ?><br>
-		Posted: <?php echo htmlentities($poll_posted); ?>; Expires: <?php echo htmlentities($poll_expires); ?>
-		
-	</div>
+	}
 	
-<?php } ?>
+	while ($dataRows = $people_content->fetch()) {
 
-<?php
+		if ($dataRows["admitted"] == true) {
+			$content[] = $dataRows;
+		}
+		
+	}
+		
+	foreach ($content as $item) {
+		
+		echo '<div class="feed-post">';
+		
+		echo '<div class="feed-heading">';
+		if ($item['type'] == 'poll') {
+			echo 'Hall of Fame Voting';
+		} else if ($item['type'] == 'person') {
+			echo 'Hall of Fame Admission';
+		}
+		echo '</div>';
+		
+		echo '<div class="feed-body">';
+		
+		echo '<span class="post-title">';
+		echo '<a href="';
+		echo $item['type'];
+		echo '.php?id=';
+		echo $item['id'];
+		echo '">';
+		if ($item['type'] == 'poll') {
+			echo $item['title'];
+		} else if ($item['type'] == 'person') {
+			echo $item['name'];
+		}
+		echo '</a>';
+		echo '</span>';
+		
+		echo '<br>';
+		
+		echo $item['intro_text'];
+		
+		echo '<br>';
+		
+		if ($item['type'] == 'poll') {
+			echo '<strong>Expires:</strong> ';
+			echo $item['expiry'];
+		} else if ($item['type'] == 'person') {
+			echo '<strong>Admitted:</strong> ';
+			echo $item['admission_date'];
+		}
+		
+		echo '</div>';
+			
+		echo '</div>';
+		
+	}
+	
 	include 'inc/footer.php';
 ?>
