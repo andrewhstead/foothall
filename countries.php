93a18b16bda7c9c1a8645e598e74bfd95983fd1b
Andrew Stead <andrew@andrewstead.co.uk>
2021-06-19 16:48:55 +0100

    Added list of countries to country menu page.

--- countries.php
+++ countries.php
@@ -14,16 +14,32 @@
 	$connectDB;
 
 	$continents = "SELECT * FROM continents";
-	$page_content = $connectDB->query($continents);
+	$continent_query = $connectDB->query($continents);
 	
 	$continent_list = array();
 	
-	while ($dataRows = $page_content->fetch()) {
+	while ($dataRows = $continent_query->fetch()) {
 
 		$continent_id = $dataRows["id"];
 		$continent_name = $dataRows["name"];
 		
 		$continent_list[] = $continent_name;
+	
+	}
+	
+	$countries = "SELECT * FROM countries";
+	$country_query = $connectDB->query($countries);
+	
+	$country_list = array();
+	
+	while ($dataRows = $country_query->fetch()) {
+
+		$country_id = $dataRows["id"];
+		$country_name = $dataRows["display_name"];
+		$country_abbreviation = $dataRows["abbreviation"];
+		$country_continent = $dataRows["continent"];
+		
+		$country_list[] = $dataRows;
 	
 	}
 ?>
@@ -37,9 +53,27 @@
 		<?php
 		
 			foreach ($continent_list as &$continent_menu) {
+				
 				echo '<h2>'.$continent_menu.'</h2>';
+		
+				foreach ($country_list as &$country_menu) {
+					
+					if ($country_menu["continent"] == $continent_menu) {
+							
+						echo '<img class="poll-icon" src="img/flags/'.strtolower($country_menu["abbreviation"]).'.png" alt="'.htmlentities($country_menu["abbreviation"]).'"> ';
+						echo $country_menu["display_name"];
+						echo '<br>';
+							
+					}
+					
+				}
+				
 			}
 		
+		?>
+		
+		<?php
+			
 		?>
 		
 	</div>
