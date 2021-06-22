4fdba288134e971b2fef5d825d050ebcb2fa6a50
Andrew Stead <andrew@andrewstead.co.uk>
2021-06-22 22:17:57 +0100

    Fixed code to display player menu page correctly.

--- players.php
+++ players.php
@@ -13,19 +13,17 @@
 						
 	$connectDB;
 
-	$countries = "SELECT * FROM countries";
-	$country_query = $connectDB->query($countries);
 	
-	
-	while ($dataRows = $country_query->fetch()) {
-
-		$country_id = $dataRows["id"];
-		$country_name = $dataRows["display_name"];
-		$country_abbreviation = $dataRows["abbreviation"];
-			
-	}
-	
-	$players = "SELECT * FROM people WHERE admitted = true AND as_player = true ORDER BY file_code";
+	$players = "
+		SELECT 
+			people.id AS player_id,
+			people.name AS player_name,
+			people.nationality AS nationality,
+			countries.display_name AS country 
+		FROM people 
+		INNER JOIN countries ON people.nationality = countries.abbreviation
+		WHERE admitted = true AND as_player = true  
+		ORDER BY nationality, file_code";
 	$player_query = $connectDB->query($players);
 	
 	$player_list = array();
@@ -33,19 +31,38 @@
 	
 	while ($dataRows = $player_query->fetch()) {
 
-		$player_id = $dataRows["id"];
-		$player_name = $dataRows["full_name"];
+		$player_id = $dataRows["player_id"];
+		$player_name = $dataRows["player_name"];
 		$nationality = $dataRows["nationality"];
+		$abbreviation = $dataRows["country"];
 		
 		$player_list[] = $dataRows;
 		
-		if (in_array($dataRows["nationality"], $country_list)) {
+		if (in_array($nationality, $country_list)) {
 			
 		} else {
-			$country_list[] = $dataRows["nationality"];
+			$country_list[] = $nationality;
 		}
 
 	}
+	
+	$countries = "SELECT * FROM countries";
+	$country_query = $connectDB->query($countries);
+	
+	$countries = array();
+	
+	while ($dataRows = $country_query->fetch()) {
+
+		$country_id = $dataRows["id"];
+		$country_name = $dataRows["display_name"];
+		$country_abbreviation = $dataRows["abbreviation"];
+			
+		if (in_array($country_abbreviation, $country_list)) {
+			$countries[] = $dataRows;
+		}
+		
+	}
+	
 ?>
 
 	<div class="page-template">
@@ -55,27 +72,31 @@
 		</h1>
 		
 		<?php
-		
-			foreach ($country_list as &$country_menu) {
-				
-				echo '<h2 class="sub-menu">'.$country_menu.'</h2>';
-				
-				echo '<div class="flex-wrapper">';
-		
-				foreach ($player_list as &$player_menu) {
 					
-					if ($player_menu["nationality"] == $country_menu) {
-						
-						echo '<div class="flex-item">';	
-						echo '<a class="standard-link" href="person.php?id='.$player_menu["id"].'">'.$player_menu["name"].'</a>';
-						echo '</div>';
+			foreach ($countries as &$country_menu) {
+				
+				echo '<div class="sub-menu">';
+				echo '<h2>';
+				echo '<img class="feed-icon" src="img/flags/'.strtolower($country_menu["abbreviation"]).'.png" alt="'.htmlentities($country_menu["display_name"]).'"> ';
+				echo $country_menu["display_name"].'</h2>';
 							
+				echo '<div class="flex-wrapper">';
+			
+					foreach ($player_list as &$player_menu) {
+							
+						if ($player_menu["nationality"] == $country_menu["abbreviation"]) {
+							
+							echo '<div class="flex-item">';	
+							echo '<a class="standard-link" href="person.php?id='.$player_menu["player_id"].'">'.$player_menu["player_name"].'</a>';
+							echo '</div>';
+							
+						}
+						
 					}
 					
-				}
-				
 				echo '</div>';
-				
+				echo '</div>';
+			
 			}
 			
 		?>
