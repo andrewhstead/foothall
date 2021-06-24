00b87c7273ba4e13f7d0ce39c94e76c89eb43476
Andrew Stead <andrew@andrewstead.co.uk>
2021-06-24 21:49:48 +0100

    Fixed tournament list for co-hosts.

--- competition.php
+++ competition.php
@@ -83,7 +83,15 @@
 						echo $tournament_menu["year"];
 					}
 					echo '</td>';
-					echo '<td><img class="poll-icon" src="img/flags/'.strtolower($tournament_menu["host"]).'.png" alt="'.strtolower($tournament_menu["host"]).'">  '.$tournament_menu["host"].'</td>';
+					echo '<td>';
+					echo '<img class="poll-icon" src="img/flags/'.strtolower($tournament_menu["host"]).'.png" alt="'.strtolower($tournament_menu["host"]).'">  '.$tournament_menu["host"];
+					if ($tournament_menu["host_2"]) {
+						echo '<br><img class="poll-icon" src="img/flags/'.strtolower($tournament_menu["host_2"]).'.png" alt="'.strtolower($tournament_menu["host_2"]).'">  '.$tournament_menu["host_2"];
+					}
+					if ($tournament_menu["host_3"]) {
+						echo '<br><img class="poll-icon" src="img/flags/'.strtolower($tournament_menu["host_3"]).'.png" alt="'.strtolower($tournament_menu["host_3"]).'">  '.$tournament_menu["host_3"];
+					}
+					echo '</td>';
 					echo '<td><img class="poll-icon" src="img/flags/'.strtolower($tournament_menu["winner"]).'.png" alt="'.strtolower($tournament_menu["winner"]).'">  '.$tournament_menu["winner"].'</td>';
 					echo '<td><img class="poll-icon" src="img/flags/'.strtolower($tournament_menu["runner_up"]).'.png" alt="'.strtolower($tournament_menu["runner_up"]).'">  '.$tournament_menu["runner_up"].'</td>';
 					echo '</tr>';
