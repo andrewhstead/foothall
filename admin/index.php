87a02c9ccf5f90b19a1b67509cebf68defbb56d3
Andrew Stead <andrew@andrewstead.co.uk>
2021-07-01 23:45:14 +0100

    Created admin folder and skeleton home page.

---
+++ admin/index.php
@@ -0,0 +1,24 @@
+<?php
+	$thispage = "Home Page";
+	
+	require_once '../inc/db.php';
+	
+	include 'inc/header.html';
+	
+	$connectDB;
+	
+?>
+
+	<div class="page-template">
+		
+		<h1>
+			Administration Area
+		</h1>
+		
+	</div>
+
+<?php
+	
+	include 'inc/footer.html';
+	
+?>
