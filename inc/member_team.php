86ae4211b90c760319ce6d15594def656db7d9e8
Andrew Stead <andrew@andrewstead.co.uk>
2021-07-21 14:09:41 +0100

    Developed person_teams model for showcasing members of teams.

--- inc/member_team.php
+++ inc/member_team.php
@@ -54,10 +54,77 @@
 			<div class="formatted-text">
 				<?php echo html_entity_decode($intro_text); ?>
 			</div>
-			
-			<div>
 				
-			</div>
+			<h2>Key Players</h2>
+			
+			<?php
+					$players = "SELECT 
+								people.id AS person_id,
+								people.name AS name,
+								people.file_code AS person_file,
+								people.active AS admitted,
+								people.picture_credit AS person_picture,
+								people.license_link AS person_license,
+								people.position AS position,
+								people.nationality AS nationality,
+								people_teams.first AS first,
+								people_teams.last AS last,
+								people_teams.appearances AS appearances,
+								people_teams.goals AS goals,
+								people_teams.summary AS summary
+								FROM people_teams
+								INNER JOIN people ON people_teams.person_id = people.id 
+								WHERE hall_team_id = '$team_id' 
+								ORDER BY file_code";
+					$players_query = $connectDB->query($players);
+														
+					while ($dataRows = $players_query->fetch()) {
+
+						$person_id = $dataRows["person_id"];
+						$person = $dataRows["name"];
+						$person_file = $dataRows["person_file"];
+						$admitted = $dataRows["admitted"];
+						$position = $dataRows["position"];
+						$nationality = $dataRows["nationality"];
+						$first = $dataRows["first"];
+						$last = $dataRows["last"];
+						$appearances = $dataRows["appearances"];
+						$goals = $dataRows["goals"];
+						$person_picture = $dataRows["person_picture"];
+						$person_license = $dataRows["person_license"];
+						$summary = $dataRows["summary"];
+						
+							echo '<div class="player-details">';
+							echo '<div class="small-frame"><img class="small-portrait" src="img/portraits/';
+							echo $person_file;
+							echo '.jpg" alt="'.$person.'">';
+							echo '<div class="copyright-info">';
+							if ($person_license) {
+								echo '<a class="menu-link" href="'.htmlentities($person_license).'">';
+							}
+							echo htmlentities($person_picture);
+							if ($person_license) {
+								echo '</a>';
+							}
+							echo '</div></div>';
+						
+						echo '<h3 class="inline-heading">';
+						if ($admitted) {
+							echo '<a class="standard-link" href="person.php?id='.$person_id.'">';
+							echo $person;
+							echo '</a>';
+						} else {
+							echo $person;
+						}
+						echo '</h3>';
+						echo $position.', '.$first.'-'.$last.'<br>';
+						echo '<strong>Appearances:</strong> '.$appearances.' <strong>Goals:</strong> '.$goals.'<br>';
+						echo '<div class="player-summary">'.$summary.'</div>';
+						echo '</div>';
+							
+					}
+						
+				?>
 			
 			<div class="formatted-text">				
 				<?php echo html_entity_decode($biography); ?>
