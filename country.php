437dbea517e6a726cb93b4c8ca52fc16fb2b2998
Andrew Stead <andrew@andrewstead.co.uk>
2021-07-26 15:16:11 +0100

    Included list of hall matches in country profile view.

--- country.php
+++ country.php
@@ -38,6 +38,10 @@
 			
 		}
 		
+	} else {
+	
+		$includes = 'none';
+		
 	}
 	
 ?>
--- inc/country_profile.php
+++ inc/country_profile.php
@@ -49,12 +49,47 @@
 			
 			while ($dataRows = $member_query->fetch()) {
 
-				$display_name = $dataRows["name"];
+				$person_name = $dataRows["name"];
 				$person_id = $dataRows["person_id"];
 				
 				echo '&#9654; <a class="standard-link" href="person.php?id='.$person_id.'">';
-				echo $display_name;
+				echo $person_name;
 				echo '</a>';
+				
+			}
+						
+			/* Checking for Hall matches and then displaying them. */
+			$matches = "SELECT 
+				matches.id as match_id,
+				matches.title as match_title
+				FROM matches
+				INNER JOIN teams team_1 ON team_1.name = matches.team_1
+				INNER JOIN teams team_2 ON team_2.name = matches.team_2
+				WHERE team_1.display_name = '$display_name'
+					OR team_2.display_name = '$display_name'
+					OR team_1.display_name = '$includes'
+					OR team_2.display_name = '$includes'";
+				
+			$match_check = $connectDB->query($matches);
+			
+			$has_matches = $match_check->fetch();
+			
+			if ($has_matches) {
+				
+				echo '<h2 class="info-page">FootHall Matches</h2>';
+			
+			}
+			
+			$match_query = $connectDB->query($matches);
+			
+			while ($dataRows = $match_query->fetch()) {
+
+				$match_id = $dataRows["match_id"];
+				$match_title = $dataRows["match_title"];
+				
+				echo '&#9654; <a class="standard-link" href="match.php?id='.$match_id.'">';
+				echo $match_title;
+				echo '</a><br>';
 				
 			}
 			
