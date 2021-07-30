4cc3ee0fa2742592f10d5371b6bce6ec96227857
Andrew Stead <andrew@andrewstead.co.uk>
2021-07-30 22:32:43 +0100

    Limited pagination links to five per page.

--- inc/pagination.php
+++ inc/pagination.php
@@ -4,42 +4,74 @@
 	<table>
 
 		<tr>
-			
-			<td>
-				<a class="pagination-link" href="<?php echo $pagination_page; ?>.php?id=1">
-					&lt;&lt;
-				</a>
-			</td>
 		
 			<?php
 			
 				$total_pages = ceil($total_items / $page_items);
+				$max_pages = 5;
 				
-				if ($total_pages > 5 && $page_id > 1) {
+				if ($page_id > 1) {
+					echo '<td><a class="pagination-link" href="'.$pagination_page.'.php?id=1">&lt;&lt;</a></td>';
+				}
+				
+				if ($page_id > 2) {
 					echo '<td><a class="pagination-link" href="'.$pagination_page.'.php?id='.$page_id - 1 .'">&lt;</a></td>';
 				}
-								
-				for ($page_number = 1; $page_number <= $total_pages; $page_number++) {
+				
+				if ($total_pages <= $max_pages) {
 					
-					if ($page_number == $page_id) {
-						echo '<td class="current-page">'.$page_number.'</td>';			
+					for ($page_number = 1; $page_number <= $total_pages; $page_number++) {
+					
+						if ($page_number == $page_id) {
+							echo '<td class="current-page">'.$page_number.'</td>';			
+						} else {
+							echo '<td><a class="pagination-link" href="'.$pagination_page.'.php?id='.$page_number.'">'.$page_number.'</a></td>';
+						}
+							
+					}
+					
+				} else {
+					
+					$iteration = 1;
+					
+					if ($page_id < 3) {
+						
+						$page_number = 1;
+					
+					} elseif ($page_id > $total_pages - 2) {
+						
+						$page_number = $total_pages - 4;
+					
 					} else {
-						echo '<td><a class="pagination-link" href="'.$pagination_page.'.php?id='.$page_number.'">'.$page_number.'</a></td>';
+						
+						$page_number = $page_id - 2;
+					
 					}
 						
-				}
+					while ($iteration <= $max_pages) {
+							
+						if ($page_number == $page_id) {
+							echo '<td class="current-page">'.$page_number.'</td>';			
+						} else {
+							echo '<td><a class="pagination-link" href="'.$pagination_page.'.php?id='.$page_number.'">'.$page_number.'</a></td>';
+						}
+							
+						$iteration++;
+						$page_number++;
+							
+					}
+					
+				}			
 				
-				if ($total_pages > 5 && $page_id < $total_pages) {
-					echo '<td><a class="pagination-link" href="'.$pagination_page.'.php?id='.$page_id + 1 .'">&gt;</a></td>';				
+				if ($page_id < $total_pages - 1) {
+					echo '<td><a class="pagination-link" href="'.$pagination_page.'.php?id='.$page_id + 1 .'">&gt;</a></td>';			
+				}			
+				
+				if ($page_id < $total_pages) {
+					echo '<td><a class="pagination-link" href="'.$pagination_page.'.php?id='.$total_pages.'">&gt;&gt;</a></td>';				
 				}
 				
 			?>
-			
-			<td>
-				<a class="pagination-link" href="<?php echo $pagination_page; ?>.php?id=<?php echo $total_pages; ?>">
-					&gt;&gt;
-				</a>
-			</td>
 			
 		</tr>
 
