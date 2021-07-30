8dcf3cbc21906d526f6c7189fc07307708674d31
Andrew Stead <andrew@andrewstead.co.uk>
2021-07-30 14:43:08 +0100

    Added pagination links to move forward and back.

--- inc/pagination.php
+++ inc/pagination.php
@@ -14,21 +14,25 @@
 			<?php
 			
 				$total_pages = ceil($total_items / $page_items);
-			
+				
+				if ($total_pages > 5 && $page_id > 1) {
+					echo '<td><a class="pagination-link" href="'.$pagination_page.'.php?id='.$page_id - 1 .'">&lt;</a></td>';
+				}
+								
 				for ($page_number = 1; $page_number <= $total_pages; $page_number++) {
 					
 					if ($page_number == $page_id) {
-						
-						echo '<td class="current-page">'.$page_number.'</td>';
-					
+						echo '<td class="current-page">'.$page_number.'</td>';			
 					} else {
-						
 						echo '<td><a class="pagination-link" href="'.$pagination_page.'.php?id='.$page_number.'">'.$page_number.'</a></td>';
-					
 					}
-					
+						
 				}
-			
+				
+				if ($total_pages > 5 && $page_id < $total_pages) {
+					echo '<td><a class="pagination-link" href="'.$pagination_page.'.php?id='.$page_id + 1 .'">&gt;</a></td>';				
+				}
+				
 			?>
 			
 			<td>
