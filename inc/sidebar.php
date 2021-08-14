58244b4693678f390d498bd19454161c9311a229
Andrew Stead <andrew@andrewstead.co.uk>
2021-08-14 14:55:42 +0100

    Added highest rated matches to sidebar.

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -21,20 +21,20 @@
 					LIMIT 10";
 					$player_leader_query = $connectDB->query($player_leaders);
 					
-					$result_count = "SELECT COUNT(*) FROM people WHERE active = true AND as_player = true";
-					$results = $connectDB->query($result_count);
-					$total_results = $results->fetchColumn();
+					$player_result_count = "SELECT COUNT(*) FROM people WHERE active = true AND as_player = true";
+					$player_results = $connectDB->query($player_result_count);
+					$total_players = $player_results->fetchColumn();
 					
-					$position = 0;
+					$player_position = 0;
 					
-					if ($total_results == 0) {
+					if ($total_players == 0) {
 						echo '<div class="centre-text">No players elected yet.</div>';
 					} else {
 						
 						echo '<table class="sidebar-table">';
 						while ($dataRows = $player_leader_query->fetch()) {
 						
-							$position++;
+							$player_position++;
 
 							$player_id = $dataRows["player_id"];
 							$player_name = $dataRows["player_name"];
@@ -43,10 +43,72 @@
 							$country = $dataRows["country"];
 							
 							echo '<tr>';
-							echo '<td><strong>'.$position.'.</strong></td>';
+							echo '<td><strong>'.$player_position.'.</strong></td>';
 							echo '<td><img class="table-icon" src="img/flags/'.strtolower($nationality).'.png" alt="'.strtolower($country).'"> ';
 							echo '<a class="sidebar-link" href="person.php?id='.$player_id.'">'.$player_name.'</a></td>';
 							echo '<td>'.$rating.'</td>';
+							echo '</tr>';
+
+						}
+						echo '</table>';
+						
+					}
+
+				?>
+			
+			<hr>
+			
+			<h2>Rating Leaders: Matches</h2>
+			
+				<?php
+					$match_leaders = "
+					SELECT
+						matches.id AS match_id,
+						matches.title AS match_title,
+						matches.votes AS match_votes,
+						matches.rating AS match_rating,
+						matches.date AS match_date,
+						team_1.abbreviation AS team_1,
+						team_2.abbreviation AS team_2,
+						matches.score_1 AS score_1,
+						matches.score_2 AS score_2
+					FROM matches
+					INNER JOIN teams team_1 on matches.team_1 = team_1.name
+					INNER JOIN teams team_2 on matches.team_2 = team_2.name
+					WHERE matches.active = true  
+					ORDER BY rating DESC, votes DESC, file_code
+					LIMIT 10";
+					$match_leader_query = $connectDB->query($match_leaders);
+					
+					$match_result_count = "SELECT COUNT(*) FROM matches WHERE active = true";
+					$match_results = $connectDB->query($match_result_count);
+					$total_matches = $match_results->fetchColumn();
+					
+					$match_position = 0;
+					
+					if ($total_matches == 0) {
+						echo '<div class="centre-text">No players elected yet.</div>';
+					} else {
+						
+						echo '<table class="sidebar-table">';
+						while ($dataRows = $match_leader_query->fetch()) {
+						
+							$match_position++;
+
+							$match_id = $dataRows["match_id"];
+							$match_title = $dataRows["match_title"];
+							$match_rating = $dataRows["match_rating"];
+							$team_1 = $dataRows["team_1"];
+							$team_2 = $dataRows["team_2"];
+							$score_1 = $dataRows["score_1"];
+							$score_2 = $dataRows["score_2"];
+							$date = new DateTime($dataRows["match_date"]);
+							
+							echo '<tr>';
+							echo '<td><strong>'.$match_position.'.</strong></td>';
+							echo '<td>';
+							echo '<a class="sidebar-link" href="match.php?id='.$match_id.'">'.$team_1.' '.$score_1.'-'.$score_2.' '.$team_2.' ('.$date->format('Y').')</a></td>';
+							echo '<td>'.$match_rating.'</td>';
 							echo '</tr>';
 
 						}
