7c68ab9ac839ffe46882ee54519509acd188ab0e
Andrew Stead <andrew@andrewstead.co.uk>
2021-08-16 20:28:18 +0100

    Added social media icons to sidebar.

--- inc/country_profile.php
+++ inc/country_profile.php
@@ -198,6 +198,8 @@
 						
 				}
 				
+				echo '</div>';
+				
 				echo '</span>';
 			
 			}
--- inc/sidebar.php
+++ inc/sidebar.php
@@ -324,4 +324,12 @@
 			
 			<hr>
 			
+			<h2>Social Media</h2>
+			
+			<div class="social-media">
+				<a href=""><img class="social-icon" src="img/icons/facebook.png" alt="Facebook"></a>
+				<a href=""><img class="social-icon" src="img/icons/twitter.png" alt="Twitter"></a>
+				<a href=""><img class="social-icon" src="img/icons/instagram.png" alt="Instagram"></a>
+			</div>
+			
 		</div>
