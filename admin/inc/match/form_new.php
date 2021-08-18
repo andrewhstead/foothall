2a689957e9eaa0357c54aa779908b84a08068f5c
Andrew Stead <andrew@andrewstead.co.uk>
2021-08-18 21:02:56 +0100

    Rewrote form and code for adding a new match to the database.

--- admin/inc/match/form_new.php
+++ admin/inc/match/form_new.php
@@ -4,40 +4,38 @@
 					
 				<div class="flex-item form-section">
 
-					<label for="file-code">File Code:</label>
-					<input type="text" name="file-code" placeholder="File Code" id="file-code"">
+					<strong>NB: If either team is not listed, go to <a class="standard-link" href="add_new.php?type=teams">Add New Teams</a> before continuing.</strong>
+					<br>
+					<label for="team-1">Team 1:</label>
+					<input type="text" name="team-1" placeholder="Team Name" id="team-1">
+					<br>
+					<label for="team-2">Team 2:</label>
+					<input type="text" name="team-2" placeholder="Team Name" id="team-2">
 					<br>
 					<label for="teams-type">Club/National:</label>
-					<input type="text" name="teams-type" placeholder="Club or National?" id="teams-type"">
-					<br><br>
-					<label for="admitted">Admitted:</label>
-					<input type="radio" name="status" id="admitted" value="admitted">
-					<label for="contender">Contender:</label>
-					<input type="radio" name="status" id="contender" value="contender">
-					<label for="admitted">Inactive:</label>
-					<input type="radio" name="status" id="inactive" value="inactive">
-					<br>
-					<label for="admission-date">Admission Date:</label>
-					<input type="date" name="admission-date" placeholder="DD-MM-YYYY" id="admission-date"">
-					<br>
-					<label for="admission-poll">Admission Poll:</label>
-					<input type="text" name="admission-poll" placeholder="Admitted in Poll..." id="admission-poll"">
-					<br><br>
-					<label for="score">Total Rating Score:</label>
-					<input type="text" name="score" placeholder="Total Rating Score..." id="score">
-					<br>
-					<label for="votes">Rating Votes:</label>
-					<input type="text" name="votes" placeholder="Rating Votes..." id="votes"">
-					<br>
-					<label for="rating">Average Rating:</label>
-					<input type="text" name="rating" placeholder="Average Rating..." id="rating"">
-				
-				</div>	
+					<input type="text" name="teams-type" placeholder="Club or National?" id="teams-type">
 					
-				<div class="flex-item form-section">
+					<br><br>
+					Score: 
+					<label for="score-1">T1</label>
+					<input type="text" name="score-1" placeholder="0" id="score-1" size="1">
+					<label for="score-2">T2</label>
+					<input type="text" name="score-2" placeholder="0" id="score-2" size="1">
+					<br>
+					<label for="extra-time">Extra Time?</label>
+					<input type="checkbox" name="extra-time" id="extra-time">
+					<label for="penalties">Penalties?</label>
+					<input type="checkbox" name="penalties" id="penalties">
+					<br>
+					Penalties Score (if applicable): 
+					<label for="score-1">T1</label>
+					<input type="text" name="score-1" placeholder="0" id="penalties-1" size="1">
+					<label for="score-2">T2</label>
+					<input type="text" name="score-2" placeholder="0" id="penalties-2" size="1">
 					
+					<br><br>					
 					<label for="match-date">Date:</label>
-					<input type="date" name="match-date" placeholder="DD-MM-YYYY" id="match-date"">
+					<input type="date" name="match-date" placeholder="DD-MM-YYYY" id="match-date">
 					<br>
 					<label for="competition">Competition:</label>
 					<select id="competition" name="competition">
@@ -53,18 +51,83 @@
 					<br>
 					<label for="stage">Stage:</label>
 					<input type="text" name="stage" placeholder="Stage" id="stage">
+					<br>
+					<label for="section">Section:</label>
+					<input type="text" name="section" placeholder="Section" id="section">
+					
 					<br><br>
-					<label for="team-1">Team 1:</label>
-					<input type="text" name="team-1" placeholder="Team Name" id="team-1">
+					<label for="file-code">File Code:</label>
+					<input type="text" name="file-code" placeholder="File Code" id="file-code">
 					<br>
-					<label for="score-1">Score:</label>
-					<input type="text" name="score-1" placeholder="0" id="score-1" size="1">
+					<label for="title">Title:</label>
+					<input type="text" name="title" placeholder="Title" id="title">
+				
+				</div>	
+					
+				<div class="flex-item form-section">
+					
+					<label for="attendance">Attendance:</label>
+					<input type="text" name="attendance" placeholder="Attendance" id="attendance">
 					<br>
-					<label for="team-2">Team 2:</label>
-					<input type="text" name="team-2" placeholder="Team Name" id="team-2">
+					<label for="stadium">Stadium:</label>
+					<input type="text" name="stadium" placeholder="Stadium" id="stadium">
 					<br>
-					<label for="score-2">Score:</label>
-					<input type="text" name="score-2" placeholder="0" id="score-2" size="1">
+					<label for="city">City:</label>
+					<input type="text" name="city" placeholder="City" id="city">
+					<br>
+					<label for="country">Country:</label>
+					<select id="country" name="country">
+					<?php
+						$countries = "SELECT * FROM countries WHERE affiliated = true OR defunct = true ORDER BY display_name";
+						$country_query = $connectDB->query($countries);
+						while ($dataRows = $country_query->fetch()) {
+							$country_name = $dataRows["display_name"];
+							$country_abbreviation = $dataRows["abbreviation"];
+							echo '<option value="'.$dataRows["abbreviation"].'">'.$dataRows["display_name"].'</option>';
+						}
+					?>	
+					</select>
+					
+					<br><br>
+					<label for="referee">Referee:</label>
+					<input type="text" name="referee" placeholder="Referee" id="referee">
+					<br>
+					<label for="nationality">Nationality:</label>
+					<select id="nationality" name="nationality">
+					<?php
+						$countries = "SELECT * FROM countries WHERE affiliated = true OR defunct = true ORDER BY display_name";
+						$country_query = $connectDB->query($countries);
+						while ($dataRows = $country_query->fetch()) {
+							$country_name = $dataRows["display_name"];
+							$country_abbreviation = $dataRows["abbreviation"];
+							echo '<option value="'.$dataRows["abbreviation"].'">'.$dataRows["display_name"].'</option>';
+						}
+					?>	
+					</select>
+					
+					<br><br>
+					<label for="admitted">Admitted:</label>
+					<input type="radio" name="status" id="admitted" value="admitted">
+					<label for="contender">Contender:</label>
+					<input type="radio" name="status" id="contender" value="contender">
+					<label for="admitted">Inactive:</label>
+					<input type="radio" name="status" id="inactive" value="inactive" checked>
+					<br>
+					<label for="admission-date">Admission Date:</label>
+					<input type="date" name="admission-date" placeholder="DD-MM-YYYY" id="admission-date">
+					<br>
+					<label for="admission-poll">Admission Poll:</label>
+					<input type="text" name="admission-poll" placeholder="Admitted in Poll..." id="admission-poll">
+					
+					<br><br>
+					<label for="score">Total Rating Score:</label>
+					<input type="text" name="score" placeholder="Total Rating Score..." id="score" value="0">
+					<br>
+					<label for="votes">Rating Votes:</label>
+					<input type="text" name="votes" placeholder="Rating Votes..." id="votes" value="0">
+					<br>
+					<label for="rating">Average Rating:</label>
+					<input type="text" name="rating" placeholder="Average Rating..." id="rating" value="0.00">
 				
 				</div>
 				
--- admin/inc/match/process_new.php
+++ admin/inc/match/process_new.php
@@ -2,8 +2,26 @@
 
 	if(isset($_POST["submit"])) {
 		
-		$new_name = $_POST["person-name"];
+		$new_team_1 = $_POST["team-1"];
+		$new_team_2 = $_POST["team-2"];
+		$new_teams = $_POST["teams-type"];
+		$new_score_1 = $_POST["score-1"];
+		$new_score_2 = $_POST["score-2"];
+		$new_extra_time = $_POST["extra-time"];
+		$new_penalties = $_POST["penalties"];
+		$new_penalties_1 = $_POST["penalties-1"];
+		$new_penalties_2 = $_POST["penalties-2"];
+		$new_date = $_POST["match-date"];
+		$new_competition = $_POST["competition"];
+		$new_stage = $_POST["stage"];
+		$new_section = $_POST["section"];
 		$new_file_code = $_POST["file-code"];
+		$new_title = $_POST["title"];
+		$new_attendance = $_POST["attendance"];
+		$new_stadium = $_POST["stadium"];
+		$new_city = $_POST["city"];
+		$new_country = $_POST["country"];
+		$new_referee = $_POST["referee"];
 		$new_nationality = $_POST["nationality"];
 		if ($_POST["status"] == "admitted") {
 			$new_admitted = true;
@@ -25,61 +43,47 @@
 		} else {
 			$new_admission_poll = $_POST["admission-poll"];
 		}
-		if (isset($_POST["as-player"])) {
-			$new_as_player = true;
-		} else {
-			$new_as_player = false;
-		}
-		if (isset($_POST["as-coach"])) {
-			$new_as_coach = true;
-		} else {
-			$new_as_coach = false;
-		}
 		$new_score = $_POST["score"];
 		$new_votes = $_POST["votes"];
 		$new_rating= $_POST["rating"];
-		$new_full_name = $_POST["full-name"];
-		$new_birth_date = $_POST["birth-date"];
-		$new_birth_place = $_POST["birth-place"];
-		$new_birth_country = $_POST["birth-country"];
-		if (isset($_POST["is-living"])) {
-			$new_is_living = true;
-		} else {
-			$new_is_living = false;
-		}
-		$new_death_date = $_POST["death-date"];
-		$new_position = $_POST["position"];
 		$new_intro_text = $_POST["intro-text"];
-		$new_picture_credit = $_POST["picture-credit"];
-		$new_biography = $_POST["biography"];
+		$new_match_report = $_POST["match-report"];
 
-		$sql = "INSERT INTO people (name, file_code, nationality, contender, active, admission_date, admission_poll, as_player, as_coach, score, votes, rating, full_name, date_of_birth, place_of_birth, country_of_birth, living, date_of_death, position, intro_text, picture_credit, biography)";
-		$sql .= "VALUES (:NewName, :NewFileCode, :NewNationality, :NewContender, :NewAdmitted, :NewAdmissionDate, :NewAdmissionPoll, :NewAsPlayer, :NewAsCoach, :NewScore, :NewVotes, :NewRating, :NewFullName, :NewBirthDate, :NewBirthPlace, :NewBirthCountry, :NewIsLiving, :NewDeathDate, :NewPosition, :NewIntroText, :NewPictureCredit, :NewBiography)";
+		$sql = "INSERT INTO people (team_1, team_2, teams, score_1, score_2, extra_time, penalties, penalties_1, penalties_2, date, competition, stage, section, file_code, title, attendance, stadium, city, country, referee, ref_nat, admitted, contender, admission_date, admission_poll, score, votes, rating, intro_text, match_report)";
+		$sql .= "VALUES (:NewTeam1, :NewTeam2, :NewTeams, :NewScore1, :NewScore2, :NewExtraTime, :NewPenalties, :NewPenalties1, :NewPenalties2, :NewDate, :NewCompetition, :NewStage, :NewSection, :NewFileCode, :NewTitle, :NewAttendance, :NewStadium, :NewCity, :NewCountry, :NewReferee, :NewRefNat, :NewAdmitted, :NewContender, :NewAdmissionDate, :NewAdmissionPoll, :NewScore, :NewVotes, :NewRating, :NewIntroText, :NewMatchReport)";
 					
 		$stmt = $connectDB->prepare($sql);
 		
-		$stmt->bindValue(':NewName', $new_name);
+		$stmt->bindValue(':NewTeam1', $new_team_1);
+		$stmt->bindValue(':NewTeam2', $new_team_2);
+		$stmt->bindValue(':NewTeams', $new_teams);
+		$stmt->bindValue(':NewScore1', $new_score_1);
+		$stmt->bindValue(':NewScore2', $new_score_2);
+		$stmt->bindValue(':NewExtraTime', $new_extra_time);
+		$stmt->bindValue(':NewPenalties', $new_penalties);
+		$stmt->bindValue(':NewPenalties1', $new_penalties_1);
+		$stmt->bindValue(':NewPenalties2', $new_penalties_2);
+		$stmt->bindValue(':NewDate', $new_date);
+		$stmt->bindValue(':NewCompetition', $new_competition);
+		$stmt->bindValue(':NewStage', $new_stage);
+		$stmt->bindValue(':NewSection', $new_section);
 		$stmt->bindValue(':NewFileCode', $new_file_code);
-		$stmt->bindValue(':NewNationality', $new_nationality);
-		$stmt->bindValue(':NewContender', $new_contender);
+		$stmt->bindValue(':NewTitle', $new_title);
+		$stmt->bindValue(':NewAttendance', $new_attendance);
+		$stmt->bindValue(':NewStadium', $new_stadium);
+		$stmt->bindValue(':NewCity', $new_city);
+		$stmt->bindValue(':NewCountry', $new_country);
+		$stmt->bindValue(':NewReferee', $new_referee);
+		$stmt->bindValue(':NewRefNat', $new_nationality);
 		$stmt->bindValue(':NewAdmitted', $new_admitted);
+		$stmt->bindValue(':NewContender', $new_contender);
 		$stmt->bindValue(':NewAdmissionDate', $new_admission_date);
 		$stmt->bindValue(':NewAdmissionPoll', $new_admission_poll);
-		$stmt->bindValue(':NewAsPlayer', $new_as_player);
-		$stmt->bindValue(':NewAsCoach', $new_as_coach);
 		$stmt->bindValue(':NewScore', $new_score);
 		$stmt->bindValue(':NewVotes', $new_votes);
 		$stmt->bindValue(':NewRating', $new_rating);
-		$stmt->bindValue(':NewFullName', $new_full_name);
-		$stmt->bindValue(':NewBirthDate', $new_birth_date);
-		$stmt->bindValue(':NewBirthPlace', $new_birth_place);
-		$stmt->bindValue(':NewBirthCountry', $new_birth_country);
-		$stmt->bindValue(':NewIsLiving', $new_is_living);
-		$stmt->bindValue(':NewDeathDate', $new_death_date);
-		$stmt->bindValue(':NewPosition', $new_position);
 		$stmt->bindValue(':NewIntroText', $new_intro_text);
-		$stmt->bindValue(':NewPictureCredit', $new_picture_credit);
-		$stmt->bindValue(':NewBiography', $new_biography);
+		$stmt->bindValue(':NewMatchReport', $new_match_report);
 
 		$execute = $stmt->execute();
 		
