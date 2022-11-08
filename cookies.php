c767005ca74db4b0b1688d09d02818cff0014231
Andrew Stead <andrew@andrewstead.co.uk>
2022-11-08 21:18:45 +0000

    Cookie policy page now allows change of setting.

--- cookies.php
+++ cookies.php
@@ -1,4 +1,5 @@
 <?php
+	ob_start();
 	$thispage = "Cookie Policy";
 	
 	require_once 'inc/db.php';
@@ -6,6 +7,20 @@
 	include 'inc/header.php';
 						
 	$connectDB;
+	
+	if(isset($_POST["amend"])) {
+		if ($_POST["amend"] == 'Delete Cookies') {
+			$past = time() - 3600;
+			foreach ($_COOKIE as $key => $value) {
+				setcookie($key, $value, $past, '/');
+				unset($_COOKIE["$key"]);
+			}
+			$_SESSION['choice'] = 'Reject Cookies';
+		} else if ($_POST["amend"] == 'Accept Cookies') {
+			setcookie('general', 'accepted', time() + (86400 * 30), "/");
+			$_COOKIE['general'] = 'accepted';
+		}
+	}
 	
 ?>
 
@@ -27,6 +42,26 @@
 			If you choose to accept, any cookie which is set will remain for a period of one month. If you choose to reject, you will not be prompted to choose again for the duration of your visit to the site. You may change your preference at any time by visiting this page, using the link in the site footer.
 		</p>
 		
+		<h3>Current Setting:</h3>
+		
+		<?php 
+			echo '<form class="cookie-form" method="post" action="">';
+			if (isset($_COOKIE['general'])) {
+				echo '<div class="cookie-setting">';
+				echo '<span class="accepted-mark">&#10003</span>'; 
+				echo '<span class="accepted-text">Accepted</span>';
+				echo '</div>';
+				echo '<br><input class="cookie-reject cookie-link" type="submit" name="amend" value="Delete Cookies">';
+			} else {
+				echo '<div class="cookie-setting">';
+				echo '<span class="rejected-mark">&#10007</span>'; 
+				echo '<span class="rejected-text">Not accepted</span>';
+				echo '</div>';
+				echo '<br><input class="cookie-accept cookie-link" type="submit" name="amend" value="Accept Cookies">';
+			}
+			echo '</form>';
+		?>
+	
 	</div>
 
 	
